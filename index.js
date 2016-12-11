c1bcab596bf758fdca73702b25eae053c8e5a7de
madoos <maurice.ronet.dominguez@gmail.com>
2016-12-11 13:32:59 +0100

    implemented basic structure

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 'use strict'
 
+const util = require('./lib/util')
+const getName = util.getval.bind(null, 'name')
+const prompts = require('./lib/prompts')
+const fileWriter = require('./lib/fileWriter')
+
+module.exports = function yoHelper (generatorName, questions, templatePath) {
+  const templateVarNames = util.isFunction(questions) ? questions().map(getName) : questions.map(getName)
+
+  return {
+    prompts: prompts(generatorName, questions),
+    fileWriter: fileWriter(templatePath, templateVarNames)
+  }
+}
